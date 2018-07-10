ac3e182a07e88b1b0159efd4a2ae8c0fd058543e
Emanuele <emanuele.vannacci@gmail.com>
2018-07-10 19:13:02 +0200

    query one flink modified for benchmark

--- src/main/scala/utils/flink/CountProcessWithState.scala
+++ src/main/scala/utils/flink/CountProcessWithState.scala
@@ -153,3 +153,66 @@ class CountWithState extends ProcessAllWindowFunction[(Long, Array[Int]), (Long,
   }
 
 }
+
+class CountWithStateAndLatency extends ProcessAllWindowFunction[(Long, Array[Int], Long), (Long, Array[Int], Float), GlobalWindow] with CheckpointedFunction {
+
+  @transient
+  private var checkpointedState: MapState[Int, Int] = _
+
+  private val bufferedElements: Array[Int] = new Array[Int](24)
+
+  private var startState: ValueState[Long] = _
+  private var start: Long = 0
+
+  private var count: Float = 0F
+  private var sum: Float = 0F
+
+
+  override def process(context: Context, elements: Iterable[(Long, Array[Int], Long)], out: Collector[(Long, Array[Int], Float)]): Unit = {
+
+    for (elem <- elements) {
+
+      if (start == 0 || start > elem._1)
+        start = elem._1
+
+      for (i <- elem._2.indices) {
+        bufferedElements(i) += elem._2(i)
+      }
+
+      count += 1
+      sum += (System.currentTimeMillis() - elem._3)
+    }
+
+    out.collect((start, bufferedElements, sum / count))
+  }
+
+  override def snapshotState(context: FunctionSnapshotContext): Unit = {
+    updateState(bufferedElements)
+    startState.update(start)
+  }
+
+  def updateState(data: Array[Int]): Unit = {
+    for (i <- data.indices) {
+      val old = checkpointedState.get(i)
+      val newVal = old + data(i)
+      checkpointedState.put(i, newVal)
+    }
+  }
+
+  override def initializeState(context: FunctionInitializationContext): Unit = {
+    val descriptor = new MapStateDescriptor[Int, Int]("buffered-elements", classOf[Int], classOf[Int])
+    checkpointedState = getRuntimeContext.getMapState(descriptor)
+
+    startState = getRuntimeContext.getState(
+      new ValueStateDescriptor[Long]("start", classOf[Long]))
+
+    if (context.isRestored) {
+      val iterator = checkpointedState.keys().iterator()
+      while (iterator.hasNext) {
+        val key = iterator.next()
+        bufferedElements(key) = checkpointedState.get(key)
+      }
+    }
+  }
+
+}
